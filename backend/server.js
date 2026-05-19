1939680571b51dc1a65b1e982c03d0a962f01dbe
smt-s5 <tydsimon09@gmail.com>
2026-05-19 12:24:51 +0700

    initialize MySQL database connection to backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('./db')
 const app = express()
 const port = 3000
 
@@ -6,6 +7,26 @@ app.get('/', (req, res) => {
   res.send('Book Library')
 })
 
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, result) => {
+    if (err) {
+      console.error('Error executing query:', err.message)
+      return
+    }
+    res.json(result)
+  })
+})
+
+app.get('/books', (req, res) => {
+  db.query('SELECT * FROM books', (err, result) => {
+    if (err) {
+      console.error('Error executing query:', err.message)
+      return
+    }
+    res.json(result)
+  })
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Book Library running on port ${port}`)
 })
